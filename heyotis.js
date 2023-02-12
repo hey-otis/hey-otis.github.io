c8d334736e5fa05e11a3147b7a8d5f1085b1ec12
Priyanshu4 <agpri123@gmail.com>
2023-02-11 22:44:13 -0500

    Improvments to heyotis

--- heyotis.js
+++ heyotis.js
@@ -1,106 +1,51 @@
-// var langs =
-//     [['Afrikaans', ['af-ZA']],
-//     ['Bahasa Indonesia', ['id-ID']],
-//     ['Bahasa Melayu', ['ms-MY']],
-//     ['Català', ['ca-ES']],
-//     ['Čeština', ['cs-CZ']],
-//     ['Deutsch', ['de-DE']],
-//     ['English', ['en-AU', 'Australia'],
-//         ['en-CA', 'Canada'],
-//         ['en-IN', 'India'],
-//         ['en-NZ', 'New Zealand'],
-//         ['en-ZA', 'South Africa'],
-//         ['en-GB', 'United Kingdom'],
-//         ['en-US', 'United States']],
-//     ['Español', ['es-AR', 'Argentina'],
-//         ['es-BO', 'Bolivia'],
-//         ['es-CL', 'Chile'],
-//         ['es-CO', 'Colombia'],
-//         ['es-CR', 'Costa Rica'],
-//         ['es-EC', 'Ecuador'],
-//         ['es-SV', 'El Salvador'],
-//         ['es-ES', 'España'],
-//         ['es-US', 'Estados Unidos'],
-//         ['es-GT', 'Guatemala'],
-//         ['es-HN', 'Honduras'],
-//         ['es-MX', 'México'],
-//         ['es-NI', 'Nicaragua'],
-//         ['es-PA', 'Panamá'],
-//         ['es-PY', 'Paraguay'],
-//         ['es-PE', 'Perú'],
-//         ['es-PR', 'Puerto Rico'],
-//         ['es-DO', 'República Dominicana'],
-//         ['es-UY', 'Uruguay'],
-//         ['es-VE', 'Venezuela']],
-//     ['Euskara', ['eu-ES']],
-//     ['Français', ['fr-FR']],
-//     ['Galego', ['gl-ES']],
-//     ['Hrvatski', ['hr_HR']],
-//     ['IsiZulu', ['zu-ZA']],
-//     ['Íslenska', ['is-IS']],
-//     ['Italiano', ['it-IT', 'Italia'],
-//         ['it-CH', 'Svizzera']],
-//     ['Magyar', ['hu-HU']],
-//     ['Nederlands', ['nl-NL']],
-//     ['Norsk bokmål', ['nb-NO']],
-//     ['Polski', ['pl-PL']],
-//     ['Português', ['pt-BR', 'Brasil'],
-//         ['pt-PT', 'Portugal']],
-//     ['Română', ['ro-RO']],
-//     ['Slovenčina', ['sk-SK']],
-//     ['Suomi', ['fi-FI']],
-//     ['Svenska', ['sv-SE']],
-//     ['Türkçe', ['tr-TR']],
-//     ['български', ['bg-BG']],
-//     ['Pусский', ['ru-RU']],
-//     ['Српски', ['sr-RS']],
-//     ['한국어', ['ko-KR']],
-//     ['中文', ['cmn-Hans-CN', '普通话 (中国大陆)'],
-//         ['cmn-Hans-HK', '普通话 (香港)'],
-//         ['cmn-Hant-TW', '中文 (台灣)'],
-//         ['yue-Hant-HK', '粵語 (香港)']],
-//     ['日本語', ['ja-JP']],
-//     ['Lingua latīna', ['la']]];
+var otis_awake = false;
+var recognition = null;
 
-// let select_language = document.querySelector('#select_language');
-// let select_dialect = document.querySelector('#select_dialect');
 
-// for (var i = 0; i < langs.length; i++) {
-//     select_language.options[i] = new Option(langs[i][0], i);
-// }
+function wake_otis() {
+    // Call this function to wake otis after hey otis is said
+    otis_awake = true;
+    document.getElementById("otis-orb").style.visibility = "visible";
+}
 
-// select_language.selectedIndex = 6;
-// updateCountry();
-// select_dialect.selectedIndex = 6;
+function sleep_otis() {
+    // Call this function to sleep otis after otis executes a command
+    otis_awake = false;
+    document.getElementById("otis-orb").style.visibility = "hidden";
+}
 
-// function updateCountry() {
-//     for (var i = select_dialect.options.length - 1; i >= 0; i--) {
-//         select_dialect.remove(i);
-//     }
-//     var list = langs[select_language.selectedIndex];
-//     for (var i = 1; i < list.length; i++) {
-//         select_dialect.options.add(new Option(list[i][1], list[i][0]));
-//     }
-//     select_dialect.style.visibility = list[1].length == 1 ? 'hidden' : 'visible';
-// }
+// otis starts asleep
+sleep_otis();
 
-function processTranscript(transcript) {
-    let processed_transcript = "";
+function listen_for_hey_otis(transcript) {
+    // returns true if hey otis was said during the transcript
     const word_array = transcript.split(" ");
-
-    var msg_start_idx = word_array.length;
     for (let i = 0; i < word_array.length - 1; i++) {
         if (word_array[i].toLowerCase() == "hey" && word_array[i + 1].toLowerCase() == "otis") {
-            processed_transcript += "Hey Otis,";
-            msg_start_idx = i + 2;
-        }
-        if (i > msg_start_idx) {
-            processed_transcript += " ";
-            processed_transcript += word_array[i];
+			console.log("found hey otis");
+            return true;
         }
     }
+    return false;
+}
 
-    return processed_transcript;
+function processFinalTranscript(transcript) {
+    // processes final transcripts
+    if (otis_awake) {
+        let response = constructResponse(transcript);
+		if (response != "")
+		{
+			speakResponse(response);
+		    sleep_otis();
+			document.querySelector("#final").innerHTML = response;
+			document.querySelector("#interim").innerHTML = "";
+		}
+    }
+    else if (!otis_awake && listen_for_hey_otis(transcript)) {
+        wake_otis();
+        document.querySelector("#final").innerHTML = "";
+		document.querySelector("#interim").innerHTML = "Hey Otis,";
+    }
 }
 
 function constructResponse(query) {
@@ -108,39 +53,47 @@ function constructResponse(query) {
     if (query.includes("directions") && query.includes("food") || query.includes("court")) {
         return "Take the elevator to floor 2 when it arrives. " +
             "When you exit the elevator, turn left and walk straight. " +
-            "You will see the food court on your right. " +
-            "Scan the QR code to take the directions with you."
-    }
-    else {
+            "You will see the food court on your right."		
+		console.log("built response");
+			
+    } else {
         return ""
     }
 }
 
 function speakResponse(response) {
-    var msg = new SpeechSynthesisUtterance();
-    msg.text = response;
-    window.speechSynthesis.speak(msg);
+	console.log("speaking response");
+	var speaker = new SpeechSynthesisUtterance();
+    speaker.text = response;
+    window.speechSynthesis.speak(speaker);
+}
+
+function setTranscriptOutput(interim, response) {
+    document.querySelector("#interim").innerHTML = interim;
+    document.querySelector("#final").innerHTML = response;
 }
 
 if ("webkitSpeechRecognition" in window) {
+	
+	
+	var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+/* 	const recognition = new SpeechRecognition();
+
+	// This runs when the speech recognition service starts
+	recognition.onstart = function () {
+		
+	};
+
+	// stop listening the speech recognition
+	recognition.onspeechend = function () {
+		//recognition.stop();
+	}
+	  
     let speechRecognition = new webkitSpeechRecognition();
     let final_transcript = "";
 
-    speechRecognition.continuous = true;
     speechRecognition.interimResults = true;
-    // speechRecognition.lang = document.querySelector("#select_dialect").value;
-
-    // speechRecognition.onstart = () => {
-    //     document.querySelector("#status").style.display = "block";
-    // };
-    // speechRecognition.onerror = () => {
-    //     document.querySelector("#status").style.display = "none";
-    //     console.log("Speech Recognition Error");
-    // };
-    // speechRecognition.onend = () => {
-    //     document.querySelector("#status").style.display = "none";
-    //     console.log("Speech Recognition Ended");
-    // };
+	speechRecognition.continuous = true;
 
     speechRecognition.onresult = (event) => {
         let interim_transcript = "";
@@ -148,28 +101,44 @@ if ("webkitSpeechRecognition" in window) {
         for (let i = event.resultIndex; i < event.results.length; ++i) {
             if (event.results[i].isFinal) {
                 final_transcript += event.results[i][0].transcript;
-                final_transcript = processTranscript(final_transcript);
-                let response = constructResponse(final_transcript);
-                speakResponse(response);
             } else {
                 interim_transcript += event.results[i][0].transcript;
-                interim_transcript = processTranscript(interim_transcript);
+
             }
         }
 
-        document.querySelector("#final").innerHTML = final_transcript;
-        document.querySelector("#interim").innerHTML = interim_transcript;
+        processFinalTranscript(final_transcript);
+		console.log("Final:", final_transcript)
+		console.log("Interim:", interim_transcript);
+		document.querySelector("#interim").innerHTML = interim_transcript;
+	
+    } */
+	
+  var recognition = new SpeechRecognition();
 
-    }
+  // This runs when the speech recognition service starts
+  recognition.onstart = function () {
+    //wake_otis();
+  };
 
-    document.addEventListener("keypress", speechRecognition.start());
+  // stop listenting the speech recognition
+  recognition.onspeechend = function () {
+    recognition.stop();
+  }
+  
+   // stop listenting the speech recognition
+  recognition.onend = function () {
+    recognition.start();
+  }
 
-    // document.querySelector("#start").onclick = () => {
-    //     speechRecognition.start();
-    // };
-    // document.querySelector("#stop").onclick = () => {
-    //     speechRecognition.stop();
-    // };
+  // This runs when the speech recognition service returns result
+  recognition.onresult = function (event) {
+    const transcript = event.results[0][0].transcript.toLowerCase();
+    const confidence = event.results[0][0].confidence;
+	processFinalTranscript(transcript);
+  }
+
+  document.addEventListener("keypress", recognition.start());
 
 } else {
     console.log("Speech Recognition Not Available");
